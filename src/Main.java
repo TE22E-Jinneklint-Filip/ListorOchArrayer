d78a8858367604249005a93d1ae4b325fbe2c4b8
Filip Jinneklint <filip.jinneklint@elev.ga.ntig.se>
2024-12-16 09:11:05 +0100

    Klar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] leksaker = {"Hotwheels", "Lego", "Fidgetspinner", "Barbie", "Rubickskub"};
+
+        String[] klassKompisar = {"Vincent", "Damjan", "Anton", "Mikeal", "Tådor"};
+
+        int[] femVärden = {1, 3, 5, 7, 9};
+
+        ArrayList<String> cities = new ArrayList<String>();
+
+        for (int i = 0; i < leksaker.length; i++) {
+            System.out.println(klassKompisar[i] + " ger " + leksaker[i] + " betyget " + femVärden[i]);
+
+        }
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Skriv in namnet på en stad: ");
+            String cityName = scan.nextLine();
+
+            if (cityName.toLowerCase().equals("exit")) {
+                break;
+            } else {
+                cities.add(cityName);
+            }
+        }
+
+        for (int i = 0; i < cities.size(); i++) {
+            System.out.println(cities.get(i));
+        }
+
     }
 }
